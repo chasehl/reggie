76c86a65b44ae7269f328f5279d21a2956c5be07
HL94566 <amwang6067@gmail.com>
2025-05-07 01:16:04 +0800

    第一次提交

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,2 +1,9 @@
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static void test() {
+        System.out.println("Test method");
+    }
 }
